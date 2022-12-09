5bccd39a956238aa2a5f39f3fb3845c782cc2173
Christian Caguicla <116904409+fretz-nu@users.noreply.github.com>
2022-12-09 09:55:34 +0800

    replaced 3 to .length for consistency

--- Code Problem Compile/Array Solve/Sample6_.java
+++ Code Problem Compile/Array Solve/Sample6_.java
@@ -4,13 +4,13 @@ public class Sample6 {
 		int[][] matrix = { { 1, 2, 3 }, { 4, 5 }, { 9, 1, 3 } };
 		int sum1 = 0;
 		int sum2 = 0;
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < matrix.length; i++) {
 			sum1 += matrix[i][0]
 
 			;
 		}
 		System.out.println("Sum of all first element are: " + sum1);
-		for (int j = 0; j < 3; j++) {
+		for (int j = 0; j < matrix.length; j++) {
 			if ((j == 0) || (j == 2)) {
 				sum2 += matrix[j][2];
 			} else {
