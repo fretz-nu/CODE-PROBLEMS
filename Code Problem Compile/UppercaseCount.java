49435d469efda05191fb0093504af8c1a810e9f0
Christian Caguicla <116904409+fretz-nu@users.noreply.github.com>
2023-01-27 11:45:15 +0800

    added comment problem

--- Code Problem Compile/UppercaseCount.java
+++ Code Problem Compile/UppercaseCount.java
@@ -1,22 +1,26 @@
+// (Count uppercase letters) Write a program that prompts the user to enter a string 
+// and displays the number of the uppercase letters in the string.
+
 import java.util.Scanner;
+
 public class UppercaseCount {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int count = 0;
 		System.out.println("Enter a String: ");
-		
+
 		String word = input.nextLine();
 		int i = 0;
-		while (word.length()>i) {
+		while (word.length() > i) {
 			char letter = word.charAt(i);
-			if (letter>='A' && letter>='Z') {
+			if (letter >= 'A' && letter >= 'Z') {
 				count++;
-			} i++;
+			}
+			i++;
 		}
-		
+
 		System.out.println("The number of uppercase letters are: " + count);
-	
 
 	}
 
